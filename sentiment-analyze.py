9ce62bf3ea87e88f25420f2317f42867fa2822c8
Ashwin Raman <a24venka@uwaterloo.ca>
2016-04-06 22:33:49 +0800

    Code cleanup

--- sentiment-analyze.py
+++ sentiment-analyze.py
@@ -109,34 +109,3 @@ classifier = nltk.NaiveBayesClassifier.train(training_set)
 print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
 
-'''
-
-# Removes "a", "the" and other neutral words that 
-# do not affect the meaning
-stop_words = set(stopwords.words("english"))
-
-ps = PorterStemmer()
-
-result_words = []
-result_numbers = []
-first_row = True
-
-for row in reader:
-	if first_row:
-		first_row = False
-		continue
-	
-	result_words = row[2].split()
-	result_numbers.append( row[0:2] + row[3:] )
-	break
-
-print(result_words)
-print(result_numbers)
-
-filtered_sentence = []
-filtered_sentence = [w for w in result_words if not w in stop_words]
-print(filtered_sentence)
-
-for w in filtered_sentence:
-	print(ps.stem(w))
-'''
