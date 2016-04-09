25057cb24d6fa9132ff1fa84b62d907c35298dd8
Ashwin Raman <a24venka@uwaterloo.ca>
2016-04-09 20:40:04 +0800

    Home Grown Sentiment Analysis

--- sentiment-analyze.py
+++ sentiment-analyze.py
@@ -8,6 +8,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+from nltk.sentiment.sentiment_analyzer import SentimentAnalyzer
+from nltk.sentiment.utils import *
 
 from nltk.classify.scikitlearn import SklearnClassifier
 import pickle
@@ -34,7 +36,7 @@ def read_reviews():
 def extract_text(reviews):
 
 	allwordlists = []
-	for i in range(len(reviews) - 100000):		
+	for i in range(len(reviews)):		
 		allwordlists.append(word_tokenize(reviews[i][2]))
 	
 	return allwordlists
@@ -42,7 +44,7 @@ def extract_text(reviews):
 def extract_allwords(reviews):
 
 	allwords = []
-	for i in range(len(reviews) - 100000):		
+	for i in range(len(reviews)):		
 		for word in word_tokenize(reviews[i][2]):
 			allwords.append(word)
 	return allwords
@@ -96,16 +98,90 @@ allwords = extract_allwords(reviews)
 processed_words = process_text(allwords)
 
 features = nltk.FreqDist(processed_words)
-features = features.most_common(300)
+features = features.most_common(1000)
 features = [key for (key, value) in features]
 
 
 featuresets = [(find_features(review[2]), review[3]) for review in reviews]
 
-testing_set = featuresets[:10000]
-training_set =  featuresets[10000:20000]
+testing_set = featuresets[130000:150000]
+training_set =  featuresets[:130000]
 
+# Run only once
 classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+#classifier_f = open("naive_bayes.pickle", "rb")
+#classifier = pickle.load(classifier_f)
+#classifier_f.close()
+
+
+# Naive Bayes
 print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
 classifier.show_most_informative_features(15)
 
+filename = "naive_bayes.pickle"
+save_classifier = open(filename, "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+# Multinomial Naive Bayes
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+filename = "multinomial_naive_bayes.pickle"
+save_classifier = open(filename, "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+
+# Bernouli Classifier
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+save_classifier = open("bernoulli_naive_bayes.pickle", "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+save_classifier = open("logistic_regression.pickle", "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+
+save_classifier = open("SGDClassifier.pickle", "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+save_classifier = open("SVCClassifier.pickle", "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+save_classifier = open("Linear_SVCClassifier.pickle", "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+
+'''
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+save_classifier = open("NU_SVCClassifier.pickle", "wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+'''
